9458a57d05ab5b40c7dbcdf3053f44efe36985fe
Subramanya Vajiraya <subramanya.v@icloud.com>
2016-09-19 22:31:48 +1000

    Final

--- SmartPointers/Graph.h
+++ SmartPointers/Graph.h
@@ -1,3 +1,18 @@
+ /////////////////////////////////////////////////////////////// 
+ //    														  //
+ //                         ###    ######   ######      ##    //
+ //                        ##      ##  ##   ##  ##     ###    //
+ //      #####    #####   ##           ##       ##      ##    //
+ //     ##       ##       #####       ##       ##       ##    //
+ //     ##        ####    ##  ##      ##       ##       ##    //
+ //     ##           ##   ##  ##     ##       ##        ##    //
+ //      #####   #####     ####      ##       ##        ##    //
+ //    														  //
+ //     		COMP6771 Assignment_3: SmartPointers		  //
+ //     					HEADER FILE					  	  //
+ //    														  //
+ ///////////////////////////////////////////////////////////////
+
 
 #ifndef GRAPH_H
 #define GRAPH_H
@@ -12,12 +27,10 @@
 #include <set>
 #include <typeinfo>
 
-#define DEBUG false
-
 namespace gdwg
-{
-	template <typename N, typename E> class Graph;
+{	
 
+	template <typename N, typename E> class Graph;
 	template <typename N, typename E> class Node_Iterator
 	{
 		public:
@@ -31,19 +44,22 @@ namespace gdwg
 
 			// value type
 			reference operator*() const {
-				//if (DEBUG) std::cout << "testing *: "<< (*it)->val_ << std::endl;
+				
 				return (*it)->val_;
 			}
+
 			// reference type
 			pointer operator->() const {
 				return &(operator*());
 			}
+
 			// pre increment
 			Node_Iterator& operator++() {
 				++it;
 				if (it==mynodes_.end()) end = true;
 				return *this;
 			}
+
 			// post increment
 			Node_Iterator operator++(int) {
 				Node_Iterator tmp = *this;
@@ -51,10 +67,10 @@ namespace gdwg
 				if (it==mynodes_.end()) end = true;
 				return tmp;
 			}
+
 			// equals
 			bool operator==(const Node_Iterator& other) const {
-				if (DEBUG) std::cout << "testing ==: " << end << " " << other.end << std::endl;
-				//std::cout << end << " " << other.end << std::endl;
+				
 				if (end && other.end) {
 					return true;
 				} else if (end || other.end) {
@@ -62,21 +78,23 @@ namespace gdwg
 				}
 				return !(operator*() < (*other.it)->val_) && !((*other.it)->val_ < operator*());
 			};
+
 			// not equals
 			bool operator!=(const Node_Iterator& other) const {
-				//if (DEBUG) std::cout << "testing !=" << std::endl;
+				
 				return !operator==(other);
 			}
 
+			// Assignment
 			Node_Iterator& operator=(const Node_Iterator &ni) {
 				mynodes_ 	= ni.mynodes_;
 				end 		= ni.end;
 				it 			= std::find(mynodes_.begin(),mynodes_.end(),*ni.it);
-				if (DEBUG) std::cout << "in copy operator" << std::endl;
+				
 				return *this;
 			}
 
-			//Node_Iterator(std::vector<std::shared_ptr<Node>> v)
+			//const
 			Node_Iterator(const std::map<N,std::shared_ptr<Node>> *nodes = nullptr)
 			{
 				// if it is an end iterator or the graph is empty
@@ -94,8 +112,27 @@ namespace gdwg
 				// sort the iterator container nodes
 					std::sort(mynodes_.begin(),mynodes_.end(),
 						[] (const std::shared_ptr<Node>& a, const std::shared_ptr<Node>& b) {
-							return a->numEdges_ != b->numEdges_ ?
-								a->numEdges_ > b->numEdges_ : a->val_ < b->val_;
+							return a->numEdges_ != b->numEdges_ ? a->numEdges_ < b->numEdges_ : a->val_ < b->val_;
+							});
+					it = mynodes_.begin();
+					end = false;
+				}
+			}
+
+			// constructor without internal sort feature ... Not the best way to implement this ... but works.
+			Node_Iterator(bool a, const std::map<N,std::shared_ptr<Node>> *nodes = nullptr)
+			{
+				// if it is an end iterator or the graph is empty
+				// create a one after the end iterator
+				if (nodes == nullptr || (*nodes).empty()) {
+					end = true;
+				// otherwise add all the elements of nodes_ to the iterator container
+				} else {
+					std::for_each((*nodes).begin(),(*nodes).end(),
+						[this] (const std::pair<N,std::shared_ptr<Node>>& p) 
+							{
+								p.second->update();
+								mynodes_.push_back(p.second);
 							});
 					it = mynodes_.begin();
 					end = false;
@@ -128,23 +165,22 @@ namespace gdwg
 				if (auto tmp = (*it)->dest.lock()) {
 					auto tmp2 = new std::pair<N,E>(tmp->val_,(*it)->val_);
 					return *tmp2; 
-					//return std::make_pair(tmp->val_,(*it)->val_);	
-					//return new std::pair<N,E>(tmp->val_,(*it)->val_);
 				}
-				throw std::runtime_error("getDest: Node DNE");
-				//if (DEBUG) std::cout << "ing *: "<< (*it)->val_ << std::endl;
-				
+				throw std::runtime_error("getDest: Node Does not exist");				
 			}
+
 			// reference type
 			pointer operator->() const {
 				return &(operator*());
 			}
+
 			// pre increment
 			Edge_Iterator& operator++() {
 				++it;
 				if (it==myedges_.end()) end = true;
 				return *this;
 			}
+
 			// post increment
 			Edge_Iterator operator++(int) {
 				Edge_Iterator tmp = *this;
@@ -152,9 +188,10 @@ namespace gdwg
 				if (it==myedges_.end()) end = true;
 				return tmp;
 			}
+
 			// equals
 			bool operator==(const Edge_Iterator& other) const {
-				//if (DEBUG) std::cout << "testing ==: " << end << " " << other.end << std::endl;
+				
 				if (end && other.end) {
 					return true;
 				} else if (end || other.end) {
@@ -162,9 +199,10 @@ namespace gdwg
 				}
 				return !(operator*().second < (*other.it)->val_) && !((*other.it)->val_ < operator*().second);
 			};
+
 			// not equals
 			bool operator!=(const Edge_Iterator& other) const {
-				//if (DEBUG) std::cout << "testing !=" << std::endl;
+				
 				return !operator==(other);
 			}
 
@@ -183,7 +221,7 @@ namespace gdwg
 								myedges_.push_back(e); }
 							});				
 					if (myedges_.empty()) {
-						if (DEBUG) std::cout << "Edge_Iterator constructor: all edges were dead" << std::endl;
+						
 						end = true;
 						
 					} else {
@@ -204,18 +242,19 @@ namespace gdwg
 				}
 			}
 
+
 		private:
 			N getDest () {
 				if (auto tmp = (*it)->dest.lock()) {
 					return tmp->val_;
 				}
-				throw std::runtime_error("getDest: Node DNE");
+				throw std::runtime_error("getDest: Node Does not exist");
 			}
+
 
 			bool end;
 			typename std::vector<std::shared_ptr<Edge>>::iterator it;
 			typename std::vector<std::shared_ptr<Edge>> myedges_;
-
 	};
 
 
